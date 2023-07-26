b4c026e3223384269a605ec1e079cdcd7d117724
fateking27 <fateking27@outlook.com>
2023-07-26 17:04:58 +0800

    sss

---
+++ src/components/day-04/具名插槽/code.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <div class="left">
+        <slot name="left"></slot>
+    </div>
+    <div class="center">{{title}}</div>
+    <div class="right">
+        <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            title:{
+
+            }
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
